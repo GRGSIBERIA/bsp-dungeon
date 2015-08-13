bd094d61382ba8b28135f84017c6eafd1878d319
grgsiberia <nanashi4129@gmail.com>
2015-08-13 12:20:15 +0900

    rename Rect to Grid

--- GridSplitter.hpp
+++ GridSplitter.hpp
@@ -1,18 +1,21 @@
 //
-//  RectSplitter.hpp
+//  GridSplitter.hpp
 //  DungeonMaker
 //
 //  Created by Eiichi Takebuchi on 2015/08/13.
 //  Copyright (c) 2015年 Eiichi Takebuchi. All rights reserved.
 //
 
-#ifndef DungeonMaker_RectSplitter_hpp
-#define DungeonMaker_RectSplitter_hpp
+#ifndef DungeonMaker_GridSplitter_hpp
+#define DungeonMaker_GridSplitter_hpp
+
+#include "Grid.hpp"
 
 namespace dungeon
 {
     class RectSplitter
     {
+    public:
         
     };
 }
