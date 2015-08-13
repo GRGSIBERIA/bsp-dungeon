805f744f8b95a250876f970a34e212e942ba484a
grgsiberia <nanashi4129@gmail.com>
2015-08-13 12:17:21 +0900

    add Spritter.hpp

---
+++ Splitter.hpp
@@ -0,0 +1,13 @@
+//
+//  Splitter.hpp
+//  DungeonMaker
+//
+//  Created by Eiichi Takebuchi on 2015/08/13.
+//  Copyright (c) 2015年 Eiichi Takebuchi. All rights reserved.
+//
+
+#ifndef DungeonMaker_Splitter_hpp
+#define DungeonMaker_Splitter_hpp
+
+
+#endif
