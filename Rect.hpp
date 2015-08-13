a69479dda0c751da8af57210acb698873a39b25e
grgsiberia <nanashi4129@gmail.com>
2015-08-13 20:35:05 +0900

    add Rect constructer

--- Rect.hpp
+++ Rect.hpp
@@ -19,6 +19,7 @@ namespace dungeon
         int width;
         int height;
         
+        Rect() : x(0), y(0), width(0), height(0) {}
         Rect(int x, int y, int w, int h) : x(x), y(y), width(w), height(h) {}
     };
 }
