22007f10a1c45e8cbda22f9e932411d4dbb0b9dd
grgsiberia <nanashi4129@gmail.com>
2015-08-13 19:58:05 +0900

    add BSPTree.hpp

---
+++ BSPTree.hpp
@@ -0,0 +1,24 @@
+//
+//  BSPTree.hpp
+//  DungeonMaker
+//
+//  Created by Eiichi Takebuchi on 2015/08/13.
+//  Copyright (c) 2015年 Eiichi Takebuchi. All rights reserved.
+//
+
+#ifndef DungeonMaker_BSPTree_hpp
+#define DungeonMaker_BSPTree_hpp
+
+#include <memory>
+
+namespace dungeon
+{
+    class BSPTree
+    {
+        
+        
+    public:
+    };
+}
+
+#endif
