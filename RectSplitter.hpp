7d7fb1bfedd15ba91d681685dbe124c81bc42aa6
grgsiberia <nanashi4129@gmail.com>
2015-08-13 12:18:57 +0900

    rename Spritter to Splitter

---
+++ RectSplitter.hpp
@@ -0,0 +1,20 @@
+//
+//  RectSplitter.hpp
+//  DungeonMaker
+//
+//  Created by Eiichi Takebuchi on 2015/08/13.
+//  Copyright (c) 2015年 Eiichi Takebuchi. All rights reserved.
+//
+
+#ifndef DungeonMaker_RectSplitter_hpp
+#define DungeonMaker_RectSplitter_hpp
+
+namespace dungeon
+{
+    class RectSplitter
+    {
+        
+    };
+}
+
+#endif
